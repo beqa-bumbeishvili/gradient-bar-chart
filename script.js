e9965368eb2dc813f05ce196935ded7f495a90f9
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-08-12 21:59:11 +0400

    display column

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var dataset = [
-    // [{ "Bakken": 3500 }, { "Barnett": 8000 }, { "Delaware": 10000 }, { "Eagle Ford": 9000 }, { "Haynesville": 5000 }, { "Marcellus": 10500 }, { "Midcontinent": 5000 }, { "Midland": 4000 }, { "Tuscaloosa Marina": 5500 }, { "Permian": 5000 }],
-    // [{ "Bakken": 1500 }, { "Barnett": 3500 }, { "Delaware": 3000 }, { "Eagle Ford": 4000 }, { "Haynesville": 5000 }, { "Marcellus": 9500 }, { "Midcontinent": 9200 }, { "Midland": 8000 }, { "Tuscaloosa Marina": 8100 }, { "Permian": 16000 }],
-    // [{ "Bakken": 1200 }, { "Barnett": 500 }, { "Delaware": 4000 }, { "Eagle Ford": 6000 }, { "Haynesville": 5500 }, { "Marcellus": 5000 }, { "Midcontinent": 5200 }, { "Midland": 6000 }, { "Tuscaloosa Marina": 7000 }, { "Permian": 4000 }],
-    // [{ "Bakken": 5000 }, { "Barnett": 6000 }, { "Delaware": 5000 }, { "Eagle Ford": 5000 }, { "Haynesville": 6000 }, { "Marcellus": 2000 }, { "Midcontinent": 3000 }, { "Midland": 16000 }, { "Tuscaloosa Marina": 8000 }, { "Permian": 4500 }],
-    // [{ "Bakken": 1100 }, { "Barnett": 6500 }, { "Delaware": 3000 }, { "Eagle Ford": 6500 }, { "Haynesville": 6000 }, { "Marcellus": 6000 }, { "Midcontinent": 6000 }, { "Midland": 6500 }, { "Tuscaloosa Marina": 6000 }, { "Permian": 12000 }],
-    // [{ "Bakken": 1500 }, { "Barnett": 6500 }, { "Delaware": 4000 }, { "Eagle Ford": 7500 }, { "Haynesville": 7000 }, { "Marcellus": 5500 }, { "Midcontinent": 6500 }, { "Midland": 6500 }, { "Tuscaloosa Marina": 7000 }, { "Permian": 7500 }],
-    // [{ "Bakken": 1000 }, { "Barnett": 4000 }, { "Delaware": 4000 }, { "Eagle Ford": 1500 }, { "Haynesville": 4000 }, { "Marcellus": 4000 }, { "Midcontinent": 3500 }, { "Midland": 3500 }, { "Tuscaloosa Marina": 4000 }, { "Permian": 8500 }],
-    [{ "Bakken": 3800 }, { "Barnett": 3000 }, { "Delaware": 4500 }, { "Eagle Ford": 3500 }, { "Haynesville": 8000 }, { "Marcellus": 20000 }, { "Midcontinent": 5000 }, { "Midland": 5000 }, { "Tuscaloosa Marina": 3500 }, { "Permian": 7500 }],
+    [{ "name": "Bakken", "value": 3500 }, { "name": "Barnett", "value": 8000 }, { "name": "Delaware", "value": 10000 }, { "name": "Eagle Ford", "value": 9000 }, { "name": "Haynesville", "value": 5000 }, { "name": "Marcellus", "value": 10500 }, { "name": "Midcontinent", "value": 5000 }, { "name": "Midland", "value": 4000 }, { "name": "Tuscaloosa Marina", "value": 5500 }, { "name": "Permian", "value": 5000 }],
+    [{ "name": "Bakken", "value": 1500 }, { "name": "Barnett", "value": 3500 }, { "name": "Delaware", "value": 3000 }, { "name": "Eagle Ford", "value": 4000 }, { "name": "Haynesville", "value": 5000 }, { "name": "Marcellus", "value": 9500 }, { "name": "Midcontinent", "value": 9200 }, { "name": "Midland", "value": 8000 }, { "name": "Tuscaloosa Marina", "value": 8100 }, { "name": "Permian", "value": 16000 }],
+    [{ "name": "Bakken", "value": 1200 }, { "name": "Barnett", "value": 500 }, { "name": "Delaware", "value": 4000 }, { "name": "Eagle Ford", "value": 6000 }, { "name": "Haynesville", "value": 5500 }, { "name": "Marcellus", "value": 5000 }, { "name": "Midcontinent", "value": 5200 }, { "name": "Midland", "value": 6000 }, { "name": "Tuscaloosa Marina", "value": 7000 }, { "name": "Permian", "value": 4000 }],
+    [{ "name": "Bakken", "value": 5000 }, { "name": "Barnett", "value": 6000 }, { "name": "Delaware", "value": 5000 }, { "name": "Eagle Ford", "value": 5000 }, { "name": "Haynesville", "value": 6000 }, { "name": "Marcellus", "value": 2000 }, { "name": "Midcontinent", "value": 3000 }, { "name": "Midland", "value": 16000 }, { "name": "Tuscaloosa Marina", "value": 8000 }, { "name": "Permian", "value": 4500 }],
+    [{ "name": "Bakken", "value": 1100 }, { "name": "Barnett", "value": 6500 }, { "name": "Delaware", "value": 3000 }, { "name": "Eagle Ford", "value": 6500 }, { "name": "Haynesville", "value": 6000 }, { "name": "Marcellus", "value": 6000 }, { "name": "Midcontinent", "value": 6000 }, { "name": "Midland", "value": 6500 }, { "name": "Tuscaloosa Marina", "value": 6000 }, { "name": "Permian", "value": 12000 }],
+    [{ "name": "Bakken", "value": 1500 }, { "name": "Barnett", "value": 6500 }, { "name": "Delaware", "value": 4000 }, { "name": "Eagle Ford", "value": 7500 }, { "name": "Haynesville", "value": 7000 }, { "name": "Marcellus", "value": 5500 }, { "name": "Midcontinent", "value": 6500 }, { "name": "Midland", "value": 6500 }, { "name": "Tuscaloosa Marina", "value": 7000 }, { "name": "Permian", "value": 7500 }],
+    [{ "name": "Bakken", "value": 1000 }, { "name": "Barnett", "value": 4000 }, { "name": "Delaware", "value": 4000 }, { "name": "Eagle Ford", "value": 1500 }, { "name": "Haynesville", "value": 4000 }, { "name": "Marcellus", "value": 4000 }, { "name": "Midcontinent", "value": 3500 }, { "name": "Midland", "value": 3500 }, { "name": "Tuscaloosa Marina", "value": 4000 }, { "name": "Permian", "value": 8500 }],
+    [{ "name": "Bakken", "value": 3800 }, { "name": "Barnett", "value": 3000 }, { "name": "Delaware", "value": 4500 }, { "name": "Eagle Ford", "value": 3500 }, { "name": "Haynesville", "value": 8000 }, { "name": "Marcellus", "value": 20000 }, { "name": "Midcontinent", "value": 5000 }, { "name": "Midland", "value": 5000 }, { "name": "Tuscaloosa Marina", "value": 3500 }, { "name": "Permian", "value": 7500 }],
 ];
 
 var svg = d3.select("body")
@@ -19,15 +19,58 @@ var group = svg
     .data(dataset)
     .enter()
     .append("g")
-    .attr("transform", "translate(" + 90 + "," + 30 + ")");
-
+    .attr("transform", "translate(" + function(d,i){return i;} + "," + 30 + ")");
 
 var rect = group.selectAll("rect")
     .data(function (d) { return d; })
     .enter()
     .append("rect")
     .attr("x", 100)
-    .attr("y", 460)
+    .attr("y", function (d, i) {
+        return 480 - (i * 40)
+    })
     .attr("width", 40)
     .attr("height", 60)
-    .attr("fill", "teal");
+    .attr("fill", function (d, i) {
+        return getProperColor(d);
+    });
+
+function getProperColor(object) {
+    color = "";
+    switch (object['name']) {
+        case "Bakken":
+            color = "#E9DF60";
+            break;
+        case "Barnett":
+            color = "#AED367";
+            break;
+        case "Delaware":
+            color = "#79C275";
+            break;
+        case "Eagle Ford":
+            color = "#4CAD82";
+            break;
+        case "Haynesville":
+            color = "#2E9789";
+            break;
+        case "Marcellus":
+            color = "#2D7F86";
+            break;
+        case "Midcontinent":
+            color = "#3A677B";
+            break;
+        case "Midland":
+            color = "#444F66";
+            break;
+        case "Tuscaloosa Marina":
+            color = "#45384D";
+            break;
+        case "Permian":
+            color = "#3C2533";
+            break;
+        default:
+            color = "teal";
+    }
+
+    return color;
+}
